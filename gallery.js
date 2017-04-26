a05a9e18de03061dae825ec348c5782f9d980c24
Unknown <khinrichs1@mail.csuchico.edu>
2017-04-25 18:19:06 -0700

    Randomization completed.

--- gallery.js
+++ gallery.js
@@ -1,6 +1,14 @@
 
-
+var chico = ["Upper Bidwell", "One Mile", "Naked Lounge", "Bear Hole", "Dutch Brothers"];
+var current = " ";
+//declaration of global variables
 function shuffleGallery()
 {
-
+  var rndm = Math.floor(Math.random() * (chico.length - 1));
+  while(current == chico[rndm])
+  {
+    rndm = Math.floor(Math.random() * (chico.length - 1));
+  }
+  current = chico[rndm];
+  console.log(chico[rndm]);
 }
